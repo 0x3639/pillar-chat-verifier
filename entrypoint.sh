dc79b4d227567393ec4b23407c5b4388237223b3
0x3639 <5406130+0x3639@users.noreply.github.com>
2023-07-23 15:56:59 -0500

    Create entrypoint.sh
    Signed-off-by: 0x3639 <5406130+0x3639@users.noreply.github.com>

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Replace placeholders in config.yaml with environment variables
+sed -i 's/${NODE}/'${NODE}'/g' /build/config.yaml
+sed -i 's/${URL}/'${URL}'/g' /build/config.yaml
+sed -i 's/${USERNAME}/'${USERNAME}'/g' /build/config.yaml
+sed -i 's/${PASSWORD}/'${PASSWORD}'/g' /build/config.yaml
+sed -i 's/${CHANNEL}/'${CHANNEL}'/g' /build/config.yaml
+
+# Start the application
+exec /build/pillar-chat-verifier
